cfdb363c428439e24fd9eba2b773dc6377207b7b
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-04-27 14:51:42 +1200

    game only takes board sizes 3 through 6

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,13 +15,26 @@ class App extends Component {
     })
   }
 
+  handleMessage = (e, message) => {
+    alert(message)
+    e.preventDefault()
+  }
+
+  handleClick = (e) => {
+    if (this.state.size % 1 !== 0) {
+      this.handleMessage(e, "Whole Numbers Only >:(")
+    } else if (this.state.size < 3 || this.state.size > 6) {
+      this.handleMessage(e, "Numbers 3 through 6 only pls >:(")
+    }
+  }
+
   render() {
     const { size } = this.state
     return (
       <div className="app">
         <h1>Choose Board Size!</h1>
         <input type="number" value={size} onChange={this.handleChange} />
-        <Link to='/game/' state={{ size }}>
+        <Link to='/game/' state={{ size }} onClick={this.handleClick}>
           <button id="size-button">Play!</button>
         </Link>
       </div>
