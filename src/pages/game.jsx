c6a90df2402e32f54f224521823fb1bb9f004d4c
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-05-05 15:11:41 +1200

    added support for touch devices

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react"
 import { connect } from 'react-redux'
 import { DndProvider } from 'react-dnd'
-import Backend from 'react-dnd-html5-backend'
+import HTML5Backend from 'react-dnd-html5-backend'
+import TouchBackend from 'react-dnd-html5-backend'
 import { Link } from 'gatsby'
 
 import Board from '../components/Board'
 import { scramble } from '../state/actions'
+
+const isTouchDevice = () => {
+  if ("ontouchstart" in window) {
+    return true;
+  }
+  return false;
+}
+
+const backendForDND = isTouchDevice() ? TouchBackend : HTML5Backend
 
 function boardStyle(size) {
   const width = (100 * size) + (2 * size)
@@ -37,7 +47,7 @@ class App extends Component {
       <div className="app">
         <p className="middle-text">VVVVV Empty Space goes Here!</p>
         <div className="board" style={boardStyle(size)}>
-          <DndProvider backend={Backend}>
+          <DndProvider backend={backendForDND}>
             <Board size={size} />
           </DndProvider>
         </div>
